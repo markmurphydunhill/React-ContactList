c54d349cd840f028d50ad1ff46104b31a843f840
Mark Murphy <markmurphydunhill@gmail.com>
2019-08-05 15:58:03 +0100

    Added Google map support

--- src/components/contactPublic/index.js
+++ src/components/contactPublic/index.js
@@ -5,8 +5,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./contactPublic.css";
 import { Link } from "react-router-dom";
 import PublicProfile from "../contactPublic/publicProfile"
+import Map from "../map/";
 
 export default ({ user }) => {
+    const location = {lng:-7.142379, lat: 52.246502} // WIT, Waterford
     const name = capitalize(`${user.name.first} ${user.name.last}`);
     return (
         <Fragment>
@@ -28,8 +30,15 @@ export default ({ user }) => {
                 <div className="col-4">
                     <PublicProfile user={user} />
                 </div>
-                <div className="col-5 bg-secondary text-light">
-                    <span>Map placeholder</span>
+                <div className="col-5" >
+                    <Map
+                        isMarkerShown
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        location={location}
+                        containerElement={<div style={{ height: `400px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
                 </div>
             </div>
         </Fragment>
